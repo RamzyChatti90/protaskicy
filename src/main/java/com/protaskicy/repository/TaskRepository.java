cf4c23a6d0fa56140fa8fb795504b1a33998e65a
Ramzi CHATTI <ramzychatti90@gmail.com>
2026-04-13 10:28:53 +0200

    [FORGE_DEV] YC-PROTAS-S1-003: src/main/java/com/protaskicy/repository/TaskRepository.java

--- src/main/java/com/protaskicy/repository/TaskRepository.java
+++ src/main/java/com/protaskicy/repository/TaskRepository.java
@@ -1,6 +1,8 @@
 package com.protaskicy.repository;
 
 import com.protaskicy.domain.Task;
+import java.util.List;
+import com.protaskicy.domain.enumeration.TaskStatus;
 import org.springframework.data.jpa.repository.*;
 import org.springframework.stereotype.Repository;
 
@@ -9,4 +11,19 @@ import org.springframework.stereotype.Repository;
  */
 @SuppressWarnings("unused")
 @Repository
-public interface TaskRepository extends JpaRepository<Task, Long> {}
+public interface TaskRepository extends JpaRepository<Task, Long> {
+    @Query("select task from Task task where task.assignedTo.login = ?#{authentication.name}")
+    List<Task> findByAssignedToIsCurrentUser();
+
+    Long countByAssignedToIsCurrentUserLogin(String login);
+
+    Long countByAssignedToIsCurrentUserLoginAndStatus(String login, TaskStatus status);
+
+    @Query("SELECT t.status, COUNT(t) FROM Task t WHERE t.assignedTo.login = ?#{authentication.name} GROUP BY t.status")
+    List<Object[]> countTasksByStatusForCurrentUser();
+
+    @Query(
+        "SELECT FUNCTION('DATE_TRUNC', 'week', t.createdAt), COUNT(t) FROM Task t WHERE t.assignedTo.login = ?#{authentication.name} AND t.status = 'DONE' GROUP BY FUNCTION('DATE_TRUNC', 'week', t.createdAt) ORDER BY FUNCTION('DATE_TRUNC', 'week', t.createdAt) ASC"
+    )
+    List<Object[]> countCompletedTasksByWeekForCurrentUser();
+}
