b17eff4441dbb7c4c0f81f212cb3f6b00e7947da
Ramzi CHATTI <ramzychatti90@gmail.com>
2026-04-13 10:28:51 +0200

    [FORGE_DEV] YC-PROTAS-S1-003: src/main/java/com/protaskicy/service/dto/TaskStatusDistributionDTO.java

---
+++ src/main/java/com/protaskicy/service/dto/TaskStatusDistributionDTO.java
@@ -0,0 +1,58 @@
+package com.protaskicy.service.dto;
+
+import com.protaskicy.domain.enumeration.TaskStatus;
+import java.io.Serializable;
+import java.util.Objects;
+
+public class TaskStatusDistributionDTO implements Serializable {
+
+    private TaskStatus status;
+    private Long count;
+
+    public TaskStatusDistributionDTO() {
+        // Empty constructor needed for Jackson
+    }
+
+    public TaskStatusDistributionDTO(TaskStatus status, Long count) {
+        this.status = status;
+        this.count = count;
+    }
+
+    public TaskStatus getStatus() {
+        return status;
+    }
+
+    public void setStatus(TaskStatus status) {
+        this.status = status;
+    }
+
+    public Long getCount() {
+        return count;
+    }
+
+    public void setCount(Long count) {
+        this.count = count;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) {
+            return true;
+        }
+        if (o == null || getClass() != o.getClass()) {
+            return false;
+        }
+        TaskStatusDistributionDTO that = (TaskStatusDistributionDTO) o;
+        return Objects.equals(status, that.status) && Objects.equals(count, that.count);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(status, count);
+    }
+
+    @Override
+    public String toString() {
+        return "TaskStatusDistributionDTO{" + "status=" + status + ", count=" + count + '}';
+    }
+}
